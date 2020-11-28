8b99a9f73342a817d72781bfbf9cc0ca532edc81
faiqshahmy <50144073+faiqshahmy@users.noreply.github.com>
2020-11-29 02:00:14 +0800

    Update secondpage.php

--- secondpage.php
+++ secondpage.php
@@ -6,7 +6,7 @@
 
 <html> 
 <head>
-<button  onclick="window.print()" style="margin:auto;display:block">Print  </button>
+<button  onclick="window.print()" style="margin:auto;display:block">Print  </button>                     //Faiq Shahmy bin Shalizam 1818897
 <link rel="stylesheet" href="design.css">
 </head>
 <?php
@@ -55,21 +55,21 @@
 		<tr> 
 			<td align="center" height="50"> 
 				<b>Monday</b></td> 
-			<td align="center" height="50" name="im" id="one">
+			<td align="center" height="50" name="im" id="one">              //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('MANAGEMENT INFO SYSTEM', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3304 MANAGEMENT INFO SYSTEM";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>																
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                 //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('BUSINESS FUNDAMENTALS', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO2201 BUSINESS FUNDAMENTALS";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                  //Faiq Shahmy bin Shalizam 1818897
 			<?php															 //AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('PROBABILITY & STATISTICS', $arraysubject)) {	 //AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3223 PROBABILITY & STATISTICS";					 //AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
@@ -79,14 +79,14 @@
 			<td rowspan="6" align="center" height="50"> 
 				<h2>L<br>U<br>N<br>C<br>H</h2> 
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                   //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('OBJECT-ORIENTED PROGRAMMING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3445 OBJECT-ORIENTED PROGRAMMING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                   //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('DATABASE PROGRAMMING', $arraysubject)) {			//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3007 DATABASE PROGRAMMING";							//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
@@ -98,35 +98,35 @@
 			<td align="center" height="50"> 
 				<b>Tuesday</b> 
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                    //Faiq Shahmy bin Shalizam 1818897
 				<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('WEB DEVELOPMENT', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3312 WEB DEVELOPMENT";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                   //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('ELEMENTS OF PROGRAMMING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO1775 ELEMENTS OF PROGRAMMING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                  //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('WEB APPLICATION DEVELOPMENT', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3305 WEB APPLICATION DEVELOPMENT";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                  //Faiq Shahmy bin Shalizam 1818897
 			<?php														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('DATA WAREHOUSING', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3115 DATA WAREHOUSING";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                  //Faiq Shahmy bin Shalizam 1818897
 			<?php														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('RISK MANAGEMENT', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3777 RISK MANAGEMENT";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
@@ -139,35 +139,35 @@
 			<td align="center" height="50"> 
 				<b>Wednesday</b> 
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                  //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('MANAGEMENT INFO SYSTEM', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3304 MANAGEMENT INFO SYSTEM";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                   //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('TECHNOPRENEURSHIP', $arraysubject)) {			//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO4555 TECHNOPRENEURSHIP";							//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                    //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('PROBABILITY & STATISTICS', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3223 PROBABILITY & STATISTICS";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                       //Faiq Shahmy bin Shalizam 1818897
 			<?php														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('GAME TECHNOLOGY', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3211 GAME TECHNOLOGY";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td  align="center" height="50">
+			<td  align="center" height="50">                                       //Faiq Shahmy bin Shalizam 1818897
 			<?php														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('DATABASE PROGRAMMING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3007 DATABASE PROGRAMMING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
@@ -179,35 +179,35 @@
 			<td align="center" height="50"> 
 				<b>Thursday</b> 
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                        //Faiq Shahmy bin Shalizam 1818897
 			<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('GAME TECHNOLOGY', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3211 GAME TECHNOLOGY";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                         //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('BUSINESS FUNDAMENTALS', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO2201 BUSINESS FUNDAMENTALS";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                          //Faiq Shahmy bin Shalizam 1818897
 			<?php														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('TECHNOPRENEURSHIP', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO4555 TECHNOPRENEURSHIP";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }														//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                             //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('OBJECT-ORIENTED PROGRAMMING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3445 OBJECT-ORIENTED PROGRAMMING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?> 
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                       //Faiq Shahmy bin Shalizam 1818897
 			<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('USRAH BUDI IV', $arraysubject)) {		//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "CCUB4330 USRAH BUDI IV";						//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
@@ -219,35 +219,35 @@
 			<td align="center" height="50"> 
 				<b>Friday</b> 
 			</td> 
-			<td  align="center" height="50">
+			<td  align="center" height="50">                                    //Faiq Shahmy bin Shalizam 1818897
 			<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('WEB DEVELOPMENT', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3312 WEB DEVELOPMENT";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                     //Faiq Shahmy bin Shalizam 1818897
 			<?php															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('ELEMENTS OF PROGRAMMING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO1775 ELEMENTS OF PROGRAMMING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }															//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                      //Faiq Shahmy bin Shalizam 1818897
 			<?php																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('WEB APPLICATION DEVELOPMENT', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3305 WEB APPLICATION DEVELOPMENT";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }																//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                      //Faiq Shahmy bin Shalizam 1818897
 			<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('DATA WAREHOUSING', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3115 DATA WAREHOUSING";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 }													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
             ?>
 			</td> 
-			<td align="center" height="50">
+			<td align="center" height="50">                                     //Faiq Shahmy bin Shalizam 1818897
 			<?php													//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 if (in_array('RISK MANAGEMENT', $arraysubject)) {	//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
                 echo "INFO3777 RISK MANAGEMENT";					//AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
