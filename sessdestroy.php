b1d785fabc6f776757668b3d3d1393cd77688c7d
SyahrilDanial <75175381+SyahrilDanial@users.noreply.github.com>
2020-11-29 20:42:47 +0800

    Update sessdestroy.php

--- sessdestroy.php
+++ sessdestroy.php
@@ -1,4 +1,4 @@
-<?php    
+<?php                                   //AHMAD SYAHRIL DANIAL BIN CHE ZAINAL 1812603
     session_start(); 
     session_destroy(); 
     header('Location:secondpage.php');  
