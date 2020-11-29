4ef6e8715a1de67bcf0fadf02ef88b5e68ae6766
Izzuddinsuhaili97 <55783309+Izzuddinsuhaili97@users.noreply.github.com>
2020-11-29 14:20:20 +0800

    Update firstpage.php

--- firstpage.php
+++ firstpage.php
@@ -12,24 +12,24 @@ block
 <!--
 content of your webpage can be static or dynamic
 -->
-<font face="arial" size="2" color="#000000">
-<form method="post" action="secondpage.php">
+<font face="arial" size="2" color="#000000">   //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
+<form method="post" action="secondpage.php">    //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
 <table>
 
 <tr>
 <td>Name</td>
 <td>:</td>
-<td><input type="text" name="name" size="30"></td></tr>
+<td><input type="text" name="name" size="30"></td></tr>  //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
 
 <tr>
 <td>Matric.no</td>
 <td>:</td>
-<td><input type="text" name="matricNo" size="30"></td></tr>
+<td><input type="text" name="matricNo" size="30"></td></tr>  //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
 
 <tr>
 <td>
-<input type="submit" value="Submit">
-<input type="reset" value="Clear"></td>
+<input type="submit" value="Submit">   //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
+<input type="reset" value="Clear"></td>  //MUHAMMAD IZZUDDIN BIN SUHAILI 1818479
 </tr>
 
 </table>
